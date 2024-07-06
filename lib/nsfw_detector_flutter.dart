7a07ca9983d3280bf3a704443bbb94d84115df20
Changho Park <hoya.develop@gmail.com>
2024-07-06 22:33:57 +0900

    feat: update nsfw_detector features

--- lib/nsfw_detector_flutter.dart
+++ lib/nsfw_detector_flutter.dart
@@ -1,7 +1,3 @@
 library nsfw_detector_flutter;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:nsfw_detector_flutter/src/nsfw_detector.dart';
---
+++ lib/src/nsfw_detector.dart
@@ -0,0 +1,95 @@
+import 'dart:io';
+
+import 'package:tflite_flutter/tflite_flutter.dart';
+import 'package:image/image.dart' as img;
+import 'dart:typed_data';
+
+class VggMean {
+  static const red = 123.68;
+  static const green = 116.779;
+  static const blue = 103.939;
+}
+
+class NsfwResult {
+  final bool isNsfw;
+  final double score;
+
+  NsfwResult(this.isNsfw, this.score);
+}
+
+class NsfwDetector {
+  static const _kInputWidth = 224;
+  static const _kInputHeight = 224;
+  static const _kModelPath = 'packages/nsfw_detector_flutter/assets/nsfw.tflite'; // original path: "assets/nsfw.tflite"
+  static const _kNSFWThreshold = 0.7;
+
+  late final Interpreter _interpreter;
+  late final double _threshold;
+  late final int _inputWidth;
+  late final int _inputHeight;
+
+  NsfwDetector._create(this._interpreter, this._threshold, this._inputWidth, this._inputHeight);
+
+  void close() {
+    _interpreter.close();
+  }
+
+  static Future<NsfwDetector> load({double threshold = _kNSFWThreshold, int inputWidth = _kInputWidth, int inputHeight = _kInputHeight}) async {
+    final interpreter = await Interpreter.fromAsset(_kModelPath);
+    return NsfwDetector._create(interpreter, threshold, inputWidth, inputHeight);
+  }
+
+  static Future<NsfwDetector> loadFromAsset(String modelAssetPath, {double threshold = _kNSFWThreshold, int inputWidth = _kInputWidth, int inputHeight = _kInputHeight}) async {
+    final interpreter = await Interpreter.fromAsset(modelAssetPath);
+    return NsfwDetector._create(interpreter, threshold, inputWidth, inputHeight);
+  }
+
+  static Future<NsfwDetector> loadFromFile(File modelFile, {double threshold = _kNSFWThreshold, int inputWidth = _kInputWidth, int inputHeight = _kInputHeight}) async {
+    final interpreter = Interpreter.fromFile(modelFile);
+    return NsfwDetector._create(interpreter, threshold, inputWidth, inputHeight);
+  }
+
+  Future<NsfwResult?> detectNSFWFromFile(File imageFile) async {
+    final image = img.decodeJpg(imageFile.readAsBytesSync());
+    return image == null ? null : await detectNSFWFromImage(image);
+  }
+
+  Future<NsfwResult?> detectNSFWFromBytes(Uint8List imageData) async {
+    final image = img.decodeImage(imageData);
+    return image == null ? null : await detectNSFWFromImage(image);
+  }
+
+  Future<NsfwResult?> detectNSFWFromImage(img.Image image) async {
+    img.Image resizedImage = img.copyResize(image, width: _kInputWidth, height: _kInputHeight);
+
+    Uint8List input = _imageToByteList(resizedImage);
+    final output = List.filled(1 * 2, 0.0).reshape([1, 2]);
+
+    _interpreter.run(input, output);
+
+    List<double> result = output.first ?? [];
+    double? score;
+    if (result.length == 2) {
+      score = result[1];
+    }
+    return score == null ? null : NsfwResult(score > _threshold, score);
+  }
+
+  Uint8List _imageToByteList(img.Image image) {
+    final buffer = Uint8List(_inputWidth * _inputHeight * 3 * 4);
+    final byteBuffer = buffer.buffer;
+    final imgData = Float32List.view(byteBuffer);
+
+    int index = 0;
+    for (var i = 0; i < _inputHeight; i++) {
+      for (var j = 0; j < _inputWidth; j++) {
+        var pixel = image.getPixel(j, i);
+        imgData[index++] = (img.getBlue(pixel) - VggMean.blue).toDouble();
+        imgData[index++] = (img.getGreen(pixel) - VggMean.green).toDouble();
+        imgData[index++] = (img.getRed(pixel) - VggMean.red).toDouble();
+      }
+    }
+
+    return buffer;
+  }
+}
